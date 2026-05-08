4e197762219c8e4dad9e1c1aa3ec8bbf6804465d
KonaLokesh <lokeshkona2@gmail.com>
2026-05-08 21:57:53 +0530

    hello

--- collector.py
+++ collector.py
@@ -1,14 +1,23 @@
 import requests
 import pandas as pd 
 import time 
+import json
 
 from datetime import datetime
-API_URL = "https://api.goscorer.com/api/v3/getSV3?key=118V"
+
+API_URL = "https://api.goscorer.com/api/v3/getSV3?key=118W"
 params = {
-    "mfkey": "118V",
+    "mfkey": "118W",
 }
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+
 
 }
+response = requests.get(API_URL,headers=headers)
+data = response.json()
+print(type(data))
+print(data.keys())
+print(json.dumps(data, indent=4))
+print(response.text)
