d3c10741479cfc2158c345135494a8cd4ab685e9
KonaLokesh <lokeshkona2@gmail.com>
2026-05-15 22:56:24 +0530

    dual api conncetions

--- collector.py
+++ collector.py
@@ -5,19 +5,36 @@ import json
 
 from datetime import datetime
 
-API_URL = "https://api.goscorer.com/api/v3/getSV3?key=118W"
-params = {
-    "mfkey": "118W",
+GOSCORER_URL = "https://api.goscorer.com/api/v3/getSV3?key=1194"
+COMMENTARY_URL= "https://content.crickapi.com/commentary/v2/getBallFeeds"
+commentary_payload = {
+    "matchKey":"1194",
+    "lastDocId": None,
+    "filters": {}
 }
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-
-
 }
-response = requests.get(API_URL,headers=headers)
+
+commentary_response = requests.post(
+    COMMENTARY_URL,
+    json=commentary_payload,headers=headers
+)
+
+commentry_data = commentary_response.json()
+print(type(commentry_data))
+print(commentry_data[0].items())
+
+response = requests.get(GOSCORER_URL,headers=headers)
 data = response.json()
 print(type(data))
-print(data.keys())
+print(data.items())
 print(json.dumps(data, indent=4))
 print(response.text)
+
+
+
+
+
+
